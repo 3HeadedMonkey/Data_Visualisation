ef0d8b98f856a22bf0326db2f087a302030b6215
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-04-28 20:00:19 +0200

    up to page 339

--- rw_visual.py
+++ rw_visual.py
@@ -5,13 +5,27 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is acrive
 while True:
     # Make a random walk, and plot the points
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
-    point_numbers = list(range(rw.num_points))
+    # Set the size of the plotting window
+    plt.figure(dpi=128,figsize=(10,6)) # figsize = (inches, inches)
 
+    # python assumes 80 pixels per inch
+
+    point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Oranges,
-                edgecolor='none', s=15)
+                edgecolor='none', s=1)
+
+    # Emphasize the first and last points
+    plt.scatter(0, 0, c='yellow', edgecolors='none',s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+                s=100)
+
+    # Remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input('Make another walk? (y/n):')
