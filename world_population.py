b890c131cfd99fbc6ff5aaccb34043d1b6f92ede
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-05-13 18:21:55 +0200

    Up to chapter 17

--- world_population.py
+++ world_population.py
@@ -1,16 +1,15 @@
 import json
 
-import pygal
+from pygal.maps.world import World
+from country_codes import get_country_code
 from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
-from country_codes import get_country_code
-
-# Load the data into a list.
+# Load the data into a list
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Build a dictionary of population data.
+# Build a dictionary of population data
 cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
@@ -20,7 +19,7 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
-# Group the countries into 3 population levels.
+# Group the countries into 3 population levels
 cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
 for cc, pop in cc_populations.items():
     if pop < 10000000:
@@ -30,14 +29,15 @@ for cc, pop in cc_populations.items():
     else:
         cc_pops_3[cc] = pop
 
-# See how many countries are in each level.        
-print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+# See how many countries are in each level
+print(len(cc_pops_1),len(cc_pops_2),len(cc_pops_3))
 
 wm_style = RS('#336699', base_style=LCS)
-wm = pygal.Worldmap(style=wm_style)
+wm = World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
-wm.add('10m-1bn', cc_pops_2)
-wm.add('>1bn', cc_pops_3)
-    
+wm.add('10-1bn', cc_pops_2)
+wm.add('>1n', cc_pops_3)
+wm.add('2010', cc_populations)
+
 wm.render_to_file('world_population.svg')
